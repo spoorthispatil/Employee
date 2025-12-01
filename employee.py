feeaad429134a949b14049b1d009d2c24eda92b3
Spoorthi s patil <spoorthipatil219@gmail.com>
2025-12-01 14:54:42 +0530

    Update employee_info to remove currency symbol
    Removed currency symbol from salary in employee_info function.

--- employee.py
+++ employee.py
@@ -1,5 +1,5 @@
 def employee_info(name, emp_id, department, salary):
-    return f"Employee Name: {name}\nEmployee ID: {emp_id}\nDepartment: {department}\nSalary: ₹{salary}"
+    return f"Employee Name: {name}\nEmployee ID: {emp_id}\nDepartment: {department}\nSalary: {salary}"
 if __name__ == "_main_":
     name = "Rahul"
     emp_id = "EMP1024"
